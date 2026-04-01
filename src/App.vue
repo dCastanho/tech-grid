f144967deb50a79e89a41eee9a3125b87ebcdb10
dCastanho <dc@cob.pt>
2026-04-01 18:03:25 +0100

    Removed unused drag

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,7 @@ import TGGrid from './components/TGGrid.vue';
 </script>
 
 <template>
-<div class="p-4 bg-linear-to-r from-gray-800 to-gray-900 w-screen h-screen relative" 
-		@mousemove="drag"
->
+<div class="p-4 bg-linear-to-r from-gray-800 to-gray-900 w-screen h-screen relative" >
   <TGGrid />
 </div>
 </template>
